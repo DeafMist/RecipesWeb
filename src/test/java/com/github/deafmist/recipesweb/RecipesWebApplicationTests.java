dd93d809c41bc70caa1bf156f48346f3dc85775b
DeafMist <romapr70@gmail.com>
2022-12-17 10:52:28 +0300

    Initial commit

---
+++ src/test/java/com/github/deafmist/recipesweb/RecipesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.deafmist.recipesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
